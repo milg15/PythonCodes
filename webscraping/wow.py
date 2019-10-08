bf70415e098a43c416cf9a02f82434bcceb9d95f
Maria Isabel Lopez <mrlpz1515@gmail.com>
2019-10-08 15:16:02 -0500

    Update wow.py
    Now search with an input shows the pets price, globally and in velen

--- webscraping/wow.py
+++ webscraping/wow.py
@@ -4,6 +4,8 @@ import pandas as pd
 import urllib3
 
 #just so it doesn't annoy me
+print ("Pet Name")
+pet_name = input()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
  
 # create an HTML Session object
@@ -18,19 +20,57 @@ pets_name = []
 pets_price = []
 pets_price25 = []
 pets_source = []
+pet_search = []
 for row in soup.findAll("tr"):
     cells = row.findAll('td')
     states=row.findAll('th') #To store second column data
     if len(cells)==4: #Only extract table body not heading
+        if pet_name.lower() == (cells[0].text).replace("\n", "").lower():
+            pet_search = [(cells[0].text).replace("\n", ""), cells[1].text, cells[2].text]
         pets_name.append((cells[0].text).replace("\n", ""))
         pets_price.append(cells[1].text)
         pets_price25.append(cells[2].text)
         pets_source.append(cells[3].text)
 
-df=pd.DataFrame(pets_name ,columns=['Name'])
-df['Price']=pets_price
-df['Price(25)']=pets_price25
-df['Source'] = pets_source
-df
+dfGlobal=pd.DataFrame(pets_name ,columns=['Name'])
+dfGlobal['Price']=pets_price
+dfGlobal['Price(25)']=pets_price25
+dfGlobal['Source'] = pets_source
+dfGlobal
 
-print (df)
+#now for velen
+session = HTMLSession()
+resp = session.get("https://www.wow-pets.com/realms/us/velen", verify=False)
+ 
+# Run JavaScript code on webpage
+resp.html.render()
+soup = BeautifulSoup(resp.html.html, "lxml")
+
+pets_name = []
+pets_price = []
+pets_price25 = []
+pets_source = []
+for row in soup.findAll("tr"):
+    cells = row.findAll('td')
+    states=row.findAll('th') #To store second column data
+    if len(cells)==4: #Only extract table body not heading
+        if pet_name.lower() == (cells[0].text).replace("\n", "").lower():
+            pet_search.append(cells[1].text)
+            pet_search.append(cells[2].text)
+        pets_name.append((cells[0].text).replace("\n", ""))
+        pets_price.append(cells[1].text)
+        pets_price25.append(cells[2].text)
+        pets_source.append(cells[3].text)
+
+dfVelen=pd.DataFrame(pets_name ,columns=['Name'])
+dfVelen['Price']=pets_price
+dfVelen['Price(25)']=pets_price25
+dfVelen['Source'] = pets_source
+dfVelen 
+
+if pet_search == []:
+    print("Pet not found!")
+else:
+    print ("Pet-Name: ", pet_search[0])
+    print ("Global: ", pet_search[1], pet_search[2])
+    print ("Local: ", pet_search[3], pet_search[4])
